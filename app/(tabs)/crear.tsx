0510f3472173f9b3ce1c818639f80dec13712004
alejpaa <alejandro.padillaa@unmsm.edu.pe>
2025-11-14 00:16:48 -0500

    feat: create Crear component for board creation

---
+++ app/(tabs)/crear.tsx
@@ -0,0 +1,10 @@
+import { Text } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+export default function Crear() {
+  return (
+    <SafeAreaView>
+        <Text className='color-blue-400'>Crear</Text>
+    </SafeAreaView>
+  );
+}
